c89c54bf13681609518abd6d10aeb48ca8c161ec
M. Maiz Nadeem <maiznadeem616@gmail.com>
2024-03-06 21:43:12 +0500

    Lint Issue Fix

--- src/components/Clients/index.tsx
+++ src/components/Clients/index.tsx
@@ -6,7 +6,7 @@ const Clients = () => {
     <section className="pb-20 dark:bg-dark">
       <div className="container px-4">
         <h2 className="mb-5 w-full text-center text-3xl font-bold leading-tight text-dark dark:text-white sm:text-[40px] sm:leading-[1.2]">
-          We've Worked With
+          We&apos;ve Worked With
         </h2>
         <div className="flex flex-wrap items-center justify-center gap-8 xl:gap-11">
           {clientsData.map((client) => (
